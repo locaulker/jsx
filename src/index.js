11bd39b5b5b7fa5ef05d1b1e37823989609a5fc0
Lawrence <locaulker@gmail.com>
2022-11-17 06:04:59 -0500

    new project setup update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ const el = document.getElementById("root")
 const root = ReactDOM.createRoot(el)
 
 function App() {
-  return <h1>Hi there!</h1>
+  // let message = "Bye there!"
+
+  // if (Math.random() > 0.5) {
+  //   message = "Hello there!"
+  // }
+
+  // const name = "Samantha"
+  // const age = 23
+
+  return <input type="number" min={5} max={10} />
 }
 
 root.render(<App />)
